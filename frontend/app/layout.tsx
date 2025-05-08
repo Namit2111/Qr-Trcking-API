992b7a9f3d1decb7a665989db12a484c8208bba6
Nmait2111 <namitjain2111@gmail.com>
2025-05-08 14:39:23 +0530

    small changes in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Qr-Small',
+  description: 'Created with love',
+  generator: 'namitjain.com',
 }
 
 export default function RootLayout({
